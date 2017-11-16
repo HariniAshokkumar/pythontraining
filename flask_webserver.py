b64cc6882d9f3d092bdd7447fed093af3d5df0ee
HariniAshokkumar <ash.harini@gmail.com>
2017-11-16 16:50:50 +0530

    edited web server

--- flask_webserver.py
+++ flask_webserver.py
@@ -1,11 +1,15 @@
 from flask import Flask,request
+import json
 app = Flask(__name__)
 
-@app.route('/user/<username>',methods=["POST"]) #restricting the method to only POST method
-def hello_user(username):
+@app.route('/user/',methods=["POST"]) #restricting the method to only POST method
+def hello_user(): #GET variables can be mentioned inside the brackets
 	print (request.form)
-	if (request.form["username"]=="harini"):
-		return 'Hello, %s! You are an authorized user' % username
+	user=request.form["username"]
+	converted_data=json.loads(request.form["data"])
+	command=converted_data["command"]
+	if (user=="harini"):
+		return 'your command is %s!' % command
 	else:
 		return 'Unauthorized user!'
 
